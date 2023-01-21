f23dae033891fce4c4b8c2fbd345a4ea8a4e1991
AlexSitohov <lilinfinitemob@gmail.com>
2023-01-21 14:29:57 +0300

    added: Dockerfile, docker-compose.yml

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/yt"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@postgres/youtube"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
