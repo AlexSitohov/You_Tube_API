c05f234ccbbf82b7e28503c6563268acb5b04844
AlexSitohov <lilinfinitemob@gmail.com>
2023-02-04 16:10:34 +0300

    added: dep

--- app/config.py
+++ app/config.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 SMTP_USER = os.getenv('SMTP_USER')
 SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+
--- app/database.py
+++ app/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import SQLALCHEMY_DATABASE_URL
 
-SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/yt"
 
 engine = create_engine(
